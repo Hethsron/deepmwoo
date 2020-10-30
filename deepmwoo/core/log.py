2e75a071adebbc85dcea1bcd28522de7f8fc176a
Boueya Hethsron <hethsronboueya@gmail.com>
2020-10-30 05:59:52 +0100

    creating core package

---
+++ deepmwoo/core/log.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+"""!
+    @file       log.py
+    @brief      
+    @details    
+    
+    @author     BOUEYA Hethsron Jedaël <hethsron-jedael.boueya@uha.fr>
+                BENOMAR Yassine <yassine.benomar@uha.fr>
+    
+    @version    0.0.1
+    @date       October, 23th 2020
+    
+    @note       For this program, we recommand to use the existing virtual
+                environment that allows you to avoid installing Python
+                packages globally which could break system tools or other projects 
+    
+    @pre        Before you can start installing or using packages in the
+                existing virtual environment, you'll need to activate it.
+                Activating this virtual environment will put the virtual
+                environment-specifi python and pip executables into your
+                shell's PATH.
+                    -   On macOS and Linux, run :
+                            source env/bin/activate
+                    -   On Windows, run :
+                            .\env\Scripts\activate
+
+    @post       If you want to switch projects or otherwise leave this virtual
+                environment, simply run :
+                            deactivate
+
+    @bug        No known bug to date
+    @warning    Misuse could cause program crash
+    @attention
+    @remark
+
+    @copyright  GPLv3+ : GNU GPL version 3 or later
+                Licencied Material - Property of Stimul’Activ®
+                © 2020 ENSISA (UHA) - All rights reserved.
+"""
+
