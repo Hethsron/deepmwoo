e164c2d8086d46f35fdd7b4b965ec46644b7a494
Boueya Hethsron <hethsronboueya@gmail.com>
2020-11-06 02:16:46 +0100

    disable all debugging logs

--- deepmwoo/__init__.py
+++ deepmwoo/__init__.py
@@ -32,3 +32,15 @@
 
 from .core import __all__ 
 from .core import __version__
+import os
+
+# Disable all debugging logs. In details
+#
+#  Level | Level for Humans | Level Description                  
+# -------|------------------|------------------------------------ 
+#  0     | DEBUG            | [Default] Print all messages       
+#  1     | INFO             | Filter out INFO messages           
+#  2     | WARNING          | Filter out INFO & WARNING messages 
+#  3     | ERROR            | Filter out all messages
+# 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
